483f2b5a4d14c5f8e9961bccdfc803a2896682b8
zeeman <133583207+infrared-o8@users.noreply.github.com>
2024-09-06 20:06:31 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import mysql.connector
 from datetime import datetime
 
-database = mysql.connector.connect(host="localhost", user = "root", password="admin", database="hospital")
+database = mysql.connector.connect(host="localhost", user = "root", password="admin", database="hospital_main")
 c = database.cursor()
