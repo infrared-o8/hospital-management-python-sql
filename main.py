c986a3862ee5263ebba42df813c3c52bf22376e6
zeeman <133583207+infrared-o8@users.noreply.github.com>
2024-09-07 13:58:42 +0300

    updated login/signup

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ database = mysql.connector.connect(host="localhost", user = "root", password="ad
 c = database.cursor()
 
 current_user_type = None
-current_user_id = None
+current_user_data = None
 
 
 def start_program():
@@ -27,42 +27,69 @@ def start_program():
         print("Input was of incorrect datatype. Try again...\n")
         start_program()
     else:
-        signin(current_user_type)
+        attain_creds(current_user_type)
 
-def signin(currentUserType):
-    global current_user_id
+def make_new_record(ordered_patient_table, patient_name):
+        new_patient_data = (int(ordered_patient_table[-1][0]) + 1, patient_name)
+
+        c.execute("INSERT into patients (PatientID, Name) values (%s, %s)", new_patient_data)
+        database.commit()
+
+        c.execute("select * from patients")
+        confirm_data = c.fetchall()
+        print("Updated patients table\n", confirm_data)
+
+        c.execute(f"select * from patients where PatientID = {new_patient_data[0]}")
+        current_user_data = c.fetchone()
+        print(current_user_data)
+
+def signup():
+    global current_user_data
+    #get required info
+    patient_name = input("Enter patient name: ")
+    #check if it already exists in patients table.
+    c.execute('select * FROM patients ORDER BY PatientID')
+    ordered_patient_table = c.fetchall()
+    (pExists, patient_record) = zampy.check_record_exists(patient_name, 1, ordered_patient_table)
+    if pExists:
+        print(f"Username already exists with patient data: {patient_record}!\nLogging in...")
+        confirm = input("Do you confirm this is your data? (Y/N): ")
+        if confirm in 'Yy':
+            current_user_data = patient_record
+        else:
+            make_new_record(ordered_patient_table, patient_name)
+    else:
+        print("User doesnt exist! Making new record...")
+        #doesnt exist, make a new record.
+        make_new_record(ordered_patient_table, patient_name)
+
+
+def login():
+    global current_user_data
+    requested_id = int(input("Enter patient ID: "))
+    c.execute(f"select * from patients where patientid = {requested_id}")
+    record = c.fetchone()
+    if record is None:
+        requSignUp = input("Requested ID doesnt exist. Sign up? (Y/N)")
+        if requSignUp in 'Yy':
+            signup()
+    else:
+        print(f"Succesfully retrieved patient data: {record}")
+        current_user_data = record
+
+def attain_creds(currentUserType):
+    global current_user_data
     if currentUserType == 'P':
         #print('current patient!')
         print("Log in or sign up as patient?")
         useridentify = int(input(zampy.make_menu_from_options(['Sign up', 'Log in'])))
         if useridentify == 1:
             print("Running sign up!")
-
-            #get required info
-            patient_name = input("Enter patient name: ")
-            #check if it already exists in patients table.
-            c.execute('select * FROM patients ORDER BY PatientID')
-            ordered_patient_table = c.fetchall()
-            (pExists, patient_record) = zampy.check_record_exists(patient_name, 1, ordered_patient_table)
-            if pExists:
-                print("Username already exists!\nRunning login...")
-                #confirm if all details are correct, if not, sign up.
-                current_user_id = patient_record[0]
-            else:
-                print("User doesnt exist! Making new record...")
-                #doesnt exist, make a new record.
-                
-                new_patient_data = (int(ordered_patient_table[-1][0]) + 1, input("Enter patient name: "))
-
-                c.execute("INSERT into patients (PatientID, Name) values (%s, %s)", new_patient_data)
-                database.commit()
-
-                c.execute("select * from patients")
-                confirm_data = c.fetchall()
-                print("Updated patients table\n", confirm_data)
+            signup()
+            
         elif useridentify == 2:
-            print("Running log in!")
-
+            print("Running login!")
+            login()
     elif currentUserType == 'D':
         print('current doctor!')
 
